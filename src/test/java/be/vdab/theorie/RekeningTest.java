71744606def7dd688aaf22fd1083bb6af08ad1b0
Maarten <maarten.vanpraet@vdabcampus.be>
2024-11-25 14:27:08 +0100

    test fixtures

--- src/test/java/be/vdab/theorie/RekeningTest.java
+++ src/test/java/be/vdab/theorie/RekeningTest.java
@@ -1,5 +1,6 @@
 package be.vdab.theorie;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -8,22 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RekeningTest {
+    private Rekening rekening;
+
+    @BeforeEach
+    void beforeEach(){
+        rekening = new Rekening();
+    }
+
     @Test
     void hetSaldoVanEenNieuweRekeningIs0Euro() {
-        var rekening = new Rekening();
         assertThat(rekening.getSaldo()).isZero();
     }
 
     @Test
     void nadatJe10EuroStortIsHetSaldo10Euro() {
-        var rekening = new Rekening();
         rekening.stort(BigDecimal.valueOf(10));
         assertThat(rekening.getSaldo()).isEqualByComparingTo("10");
     }
 
     @Test
     void nadatJe10EuroEn1EuroStortIsHetSaldo11Euro() {
-        var rekening = new Rekening();
         rekening.stort(BigDecimal.valueOf(10));
         rekening.stort(BigDecimal.ONE);
         assertThat(rekening.getSaldo()).isEqualByComparingTo("11");
